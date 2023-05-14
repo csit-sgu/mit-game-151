ac8b85414cff7a498198ea51403c42a73a472e61
citkniit <129386508+citkniit@users.noreply.github.com>
2023-05-14 11:38:44 +0300

    UpdateBullet (#31)
    * UpdateBullet
    
    * UpdateBullet
    
    * UpdateBullet

--- user.cpp
+++ user.cpp
@@ -376,6 +376,12 @@ void ShootBullet(Context &ctx, Object &player, float dt)
 //
 void UpdateBullet(Context &ctx, Object &obj, float dt)
 {
+    obj.bullet.position = obj.bullet.speed * dt + obj.bullet.position;
+    obj.bullet.lifetime += dt;
+    if (obj.bullet.lifetime > obj.bullet.max_lifetime) {
+        Destroy(ctx, obj);
+    }
+
 }
 
 // Задание KillEnemies.
