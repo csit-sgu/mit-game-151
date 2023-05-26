da9cf4ee516e4c0fe3fe68694998dc66f52da9a8
Anastasia Dorokhina <sota54@mail.ru>
2023-05-26 18:32:59 +0400

    Реализована функция DrawFinishScreen

--- user.cpp
+++ user.cpp
@@ -531,6 +531,14 @@ void DrawGameOverScreen(Context &ctx)
 //
 void DrawFinishScreen(Context &ctx)
 {
+    const char* finish_text_message = "WON!";
+
+    int inscription_size = MeasureText(finish_text_message, 60);
+    int point_x = ctx.screen_size.x;
+    int point_y = ctx.screen_size.y;
+
+    DrawRectangle(point_x / 4, point_y / 3, point_x / 2, point_y / 3, BLACK);
+    DrawText(finish_text_message, point_x / 2 - inscription_size / 2, point_y / 3, 60, GREEN);
 }
 
 // Задание DrawMainScreen.
