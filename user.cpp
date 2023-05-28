c9bf6847557bdb6e7a2c3bfac0592b720bbaf3a0
Optoed <origin-optoed.ru>
2023-05-28 11:37:12 +0400

    Исправлена проблемас определением ctx.screenHeight и ctx.screenWidth - задал параметры экрана 1920*1080

--- user.cpp
+++ user.cpp
@@ -539,6 +539,23 @@ void DrawDeathScreen(Context &ctx)
 //
 void DrawGameOverScreen(Context &ctx)
 {
+    int screenHeight = 1920, screeenWidth = 1080;
+
+    const char* GameOverText = "wasted";
+    int GameOverTextWidth = MeasureText(GameOverText, 45);
+
+    // Measure string width for default font
+    MeasureText(GameOverText, GameOverTextWidth);
+
+    // Draw a color-filled rectangle (with a black background)
+    DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+
+    // Calculate the position to center the text on the screen
+    int posX = (screenWidth - GameOverTextWidth) / 2;
+    int posY = screenHeight / 2;
+
+    // Draw the text in red color
+    DrawText(GameOverText, posX, posY, 45, RED);
 }
 
 // Задание DrawFinishScreen.
